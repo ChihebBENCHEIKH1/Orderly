b7c7fec217913e78f591adfc2136705c96b8a2ba
chiheb ben cheikh <bencheikh.chiheb@gmail.com>
2025-05-24 17:27:35 +0100

    added finance-service

--- order-service/src/main/java/io/chiheb/orderservice/order/listeners/StockEventsListener.java
+++ order-service/src/main/java/io/chiheb/orderservice/order/listeners/StockEventsListener.java
@@ -1,6 +1,10 @@
 package io.chiheb.orderservice.order.listeners;
 
-import io.chiheb.orderservice.common.configs.KafkaConfig;
+import static io.chiheb.orderservice.common.configs.KafkaConfig.ORDER_STOCK_CONFIRM_TOPIC;
+import static io.chiheb.orderservice.common.configs.KafkaConfig.ORDER_STOCK_REJECT_TOPIC;
+import static io.chiheb.orderservice.order.domain.OrderStatus.CANCELLED_OUT_OF_STOCK;
+import static io.chiheb.orderservice.order.domain.OrderStatus.STOCK_RESERVED;
+
 import io.chiheb.orderservice.order.OrderService;
 import io.chiheb.orderservice.order.listeners.events.StockConfirmationEvent;
 import io.chiheb.orderservice.order.listeners.events.StockRejectionEvent;
@@ -16,15 +20,21 @@ import org.springframework.stereotype.Component;
 public class StockEventsListener {
   private final OrderService orderService;
 
-  @KafkaListener(topics = KafkaConfig.ORDER_STOCK_CONFIRM_TOPIC)
+  @KafkaListener(topics = ORDER_STOCK_CONFIRM_TOPIC)
   public void confirmStock(@Payload Object stockConfirmationEvent) {
     var event = (StockConfirmationEvent) stockConfirmationEvent;
     log.info("received stock confirmation event for order {}", event.getOrderId());
+    orderService.updateStatus(event.getOrderId(), STOCK_RESERVED)
+        .doOnNext(orderService::processPayment)
+        .subscribe();
   }
 
-  @KafkaListener(topics = KafkaConfig.ORDER_STOCK_REJECT_TOPIC)
+  @KafkaListener(topics = ORDER_STOCK_REJECT_TOPIC)
   public void rejectStock(@Payload Object stockRejectionEvent) {
     var event = (StockRejectionEvent) stockRejectionEvent;
-    log.info("received stock rejection event for order {}", event.getOrderId());
+    log.info("received stock rejection event for order {}: {}", event.getOrderId(), event.getMessage());
+    orderService.updateStatus(event.getOrderId(), CANCELLED_OUT_OF_STOCK)
+        .doOnNext(orderService::notifyCustomer)
+        .subscribe();
   }
 }
